ff2d4bb3a78bb909e3b1615fcfd800b855975760
Robert Stoelhorst <robertstoelhorst@gmail.com>
2022-09-21 17:32:25 +1000

    yarn lock

--- src/components/class-components/App.js
+++ src/components/class-components/App.js
@@ -29,7 +29,7 @@ class App extends Component {
         return { loading: true };
       },
       () => {
-        console.log(this.state.loading);
+        // console.log(this.state.loading);
       }
     );
 
