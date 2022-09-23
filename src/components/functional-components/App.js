4c55e651cd65c33f45cbc765599d635be489128c
Robert Stoelhorst <robertstoelhorst@gmail.com>
2022-09-23 11:34:51 +1000

    wip

--- src/components/functional-components/App.js
+++ src/components/functional-components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import SearchBox from '../functional-components/SearchBox/SearchBox';
 import CardList from '../functional-components/CardList/CardList';
@@ -8,22 +8,39 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [searchField, setSearchField] = useState('');
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+  // const [stringField, setStringField] = useState('');
   // console.log({ searchField });
-
   console.log('render');
 
-  // fetch('https://jsonplaceholder.typicode.com/users')
-  //   .then((res) => res.json())
-  //   .then((users) => setMonsters(users));
+  // Do the fetch data
+  useEffect(() => {
+    console.log('requesting...');
+    setLoading(true);
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((users) => {
+        if (users) {
+          setMonsters(users);
+        }
+        setLoading(false);
+      });
+  }, []);
+
+  // Do the filter without a new request on state change
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
+    setFilteredMonsters(newFilteredMonsters);
+    console.log('effect is firing');
+  }, [monsters, searchField]);
 
   const onSearchChange = (e) => {
     const searchFieldString = e.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };
-
-  const filteredMonsters = monsters.filter((monster) => {
-    return monster.name.toLowerCase().includes(searchField);
-  });
 
   return (
     <div className="App">
